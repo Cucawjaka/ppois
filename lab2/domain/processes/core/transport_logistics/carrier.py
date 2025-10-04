fd815d2aeaf0cab9f08831d7c8474ef11e25a846
Cucawjaka <tvatva2006@gmail.com>
2025-10-04 09:36:26 +0300

    feat: add WeightMap, Vehicle, Shipment, Route, LogisticsDepartment, Carrier classes

---
+++ lab2/domain/processes/core/transport_logistics/carrier.py
@@ -0,0 +1,32 @@
+from core.exceptions import VehicleError, VehicleNotFoundError
+from domain.processes.core.transport_logistics.shipment import Shipment
+from domain.processes.core.transport_logistics.vehicle import Vehicle
+from domain.processes.core.warehouse_logistics.cargo import Cargo
+
+
+class Carrier:
+    def __init__(self, name: str) -> None:
+        self._name: str = name
+        self._vehicles: list[Vehicle] = list()
+        self._shipments: list[Shipment] = list()
+
+
+    def register_vehicle(self, vehicle: Vehicle) -> None:
+        self._vehicles.append(vehicle)
+
+
+    def remove_vehicle(self, vehicle: Vehicle) -> None:
+        if vehicle not in self._vehicles:
+            raise VehicleNotFoundError(f"транспорт {vehicle.vehicle_number} не найден")
+        self._vehicles.remove(vehicle)
+
+
+    def choose_vehicle(self, cargo: Cargo) -> Vehicle:
+        for vehicle in self._vehicles:
+            if not vehicle.is_busy and vehicle.can_carry(cargo):
+                return vehicle
+        raise VehicleError(f"{self._name} не имеет свободного транспорта")
+
+
+    def add_shipment(self, shipment: Shipment) -> None:
+        self._shipments.append(shipment)
---
+++ lab2/domain/processes/core/transport_logistics/logistics_department.py
@@ -0,0 +1,54 @@
+from core.enums.country import Country
+from core.exceptions import ShipmentNotFoundError, VehicleError
+from core.utils.id_generator import IDGenerator
+from domain.processes.core.transport_logistics.carrier import Carrier
+from domain.processes.core.transport_logistics.route import Route
+from domain.processes.core.transport_logistics.shipment import Shipment
+from domain.processes.core.transport_logistics.vehicle import Vehicle
+from domain.processes.core.warehouse_logistics.cargo import Cargo
+from domain.processes.core.warehouse_logistics.logistic_center import LogisticsCenter
+
+
+class LogisticsDepartment:
+    def __init__(self) -> None:
+        self._id: str = IDGenerator.create_uuid()
+        self._centers: dict[Country, LogisticsCenter] = dict()
+        self._carriers: list[Carrier] = list()     
+        self._shipments: dict[str, Shipment] = dict()      
+
+
+    def add_center(self, center: LogisticsCenter) -> None:
+        self._centers[center.country] = center
+
+
+    def add_carrier(self, carrier: Carrier) -> None:
+        self._carriers.append(carrier)
+
+
+    def plan_delivery(self, destination: LogisticsCenter, origin: LogisticsCenter, checkpoints: list[LogisticsCenter] | None) -> Route:
+        route = Route(origin, destination, checkpoints) 
+        return route
+
+
+    def start_delivery(self, cargo: Cargo, route: Route) -> None:
+        for carrier in self._carriers:
+            try:
+                vehicle: Vehicle = carrier.choose_vehicle(cargo)
+            except VehicleError:
+                continue
+
+        if not vehicle:
+            raise VehicleError("Нет доступного перевозчика для маршрута")
+
+        shipment = Shipment(cargo, route, vehicle)
+        carrier.add_shipment(shipment)
+        self._shipments[shipment.id] = shipment
+        shipment.start()
+
+
+    def track_cargo(self, shipment_id: str) -> str:
+        """Вернуть текущий статус груза (по всем маршрутам)."""
+        shipment: Shipment | None = self._shipments.get(shipment_id, None)
+        if not shipment:
+            raise ShipmentNotFoundError(f"Перевозка с id {shipment_id} не найдена")
+        return shipment.status
