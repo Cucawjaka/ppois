973ccbe992521f110eb556d9fff0b46d6ed300fb
Cucawjaka <tvatva2006@gmail.com>
2025-10-10 19:42:34 +0300

    test: add tests for Transport classes

--- lab2/tests/domain_tests/main_procceses_tests/conftest.py
+++ lab2/tests/domain_tests/main_procceses_tests/conftest.py
@@ -1,9 +1,24 @@
 import pytest
 
 from core.enums.country import Country
+from core.enums.material import Material
+from core.enums.product import Product
+from domain.processes.main_procceses.warehouse_logistics.cargo import Cargo
 from domain.processes.main_procceses.warehouse_logistics.warehouse import WareHouse
 
 
 @pytest.fixture
 def warehouse() -> WareHouse:
     return WareHouse(Country.BELARUS)
+
+
+@pytest.fixture
+def cargo() -> Cargo:
+    new_cargo: Cargo = Cargo(
+        items={Material.STEEL: 5, Product.CAR: 5},
+        origin="from_warehouse",
+        destination="to_warehouse",
+        customs_status="custom_required",
+    )
+
+    return new_cargo
--- lab2/tests/domain_tests/main_procceses_tests/warehouse_logistics_tests/conftest.py
+++ lab2/tests/domain_tests/main_procceses_tests/warehouse_logistics_tests/conftest.py
@@ -3,7 +3,6 @@ import pytest
 from core.enums.country import Country
 from core.enums.material import Material
 from core.enums.product import Product
-from domain.processes.main_procceses.warehouse_logistics.cargo import Cargo
 from domain.processes.main_procceses.warehouse_logistics.cargo_sorting_plan import (
     CargoSortingPlan,
 )
@@ -16,18 +15,6 @@ from domain.processes.main_procceses.warehouse_logistics.warehouse import WareHo
 @pytest.fixture
 def logistic_center() -> LogisticsCenter:
     return LogisticsCenter(Country.BELARUS)
-
-
-@pytest.fixture
-def cargo() -> Cargo:
-    new_cargo: Cargo = Cargo(
-        items={Material.STEEL: 5, Product.CAR: 5},
-        origin="from_warehouse",
-        destination="to_warehouse",
-        customs_status="custom_required",
-    )
-
-    return new_cargo
 
 
 @pytest.fixture
